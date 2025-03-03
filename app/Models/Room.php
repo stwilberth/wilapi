5e11ff2c45a076194daf21de648c775e2c308bac
Wilberth Loría <stwilberth@gmail.com>
2025-03-03 10:48:43 -0600

    fix room 2

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -9,11 +9,23 @@ use Illuminate\Support\Str;
 class Room extends Model
 {
     protected $fillable = [
-        'name', 'type', 'price_per_night', 
+        'name', 'number', 'type', 'price_per_night', 
         'capacity', 'beds', 'description', 'short_description',
         'amenities', 'cover_image', 'status', 
         'company_id', 'user_id'
     ];
+
+    //add current user id to room
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($room) {
+            if (!$room->user_id && auth()->check()) {
+                $room->user_id = auth()->id();
+            }
+        });
+    }
 
     protected $casts = [
         'amenities' => 'array'
