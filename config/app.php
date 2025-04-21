de502d8bfc9944432bcffdc9ac051e3f48544bbb
Wilberth Loría <stwilberth@gmail.com>
2025-04-21 12:31:07 -0600

    inv

--- config/app.php
+++ config/app.php
@@ -169,7 +169,6 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\Filament\PanelPanelProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Intervention\Image\ImageServiceProvider::class,
     ])->toArray(),
 
     /*
